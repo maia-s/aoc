23ade9390ef9c741ecae6bef7ec11f16208acce1
Maia <66437537+maia-s@users.noreply.github.com>
2020-12-01 13:28:20 +0100

    collect the parts in one file

--- src/bin/day-1.rs
+++ src/bin/day-1.rs
@@ -1,10 +1,17 @@
 const INPUT: &str = include_str!("day-1.input");
 
 fn main() {
+    part_1();
+    part_2();
+}
+
+fn part_1() {
+    println!("=[ part 1 ]=");
+
     let mut sums = [-1; 2020];
 
     for line in INPUT.lines() {
-        let i: i32 = line.parse().expect("integer expected");
+        let i: i32 = line.parse().expect("expected i32");
         let other = sums[i as usize];
         if other >= 0 {
             println!("{} + {} = {}", other, i, other + i);
@@ -14,5 +21,36 @@ fn main() {
         sums[2020 - i as usize] = i;
     }
 
-    eprintln!("not found");
+    println!("not found");
+}
+
+fn part_2() {
+    println!("\n=[ part 2 ]=");
+
+    let mut nums = Vec::<i32>::new();
+
+    for line in INPUT.lines() {
+        nums.push(line.parse().expect("expected i32"));
+    }
+
+    let mut sums = [None; 2020];
+
+    for &i in nums.iter() {
+        for &j in nums.iter() {
+            let ij = i + j;
+            if ij < 2020 {
+                sums[2020 - ij as usize] = Some((i, j));
+            }
+        }
+    }
+
+    for &i in nums.iter() {
+        if let Some((a, b)) = sums[i as usize] {
+            println!("{} + {} + {} = {}", a, b, i, a + b + i);
+            println!("{} * {} * {} = {}", a, b, i, a * b * i);
+            return;
+        }
+    }
+
+    println!("not found");
 }
