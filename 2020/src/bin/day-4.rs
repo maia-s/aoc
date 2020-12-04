89795d1c303d6edde91ea05eee8feff13425c853
Maia <66437537+maia-s@users.noreply.github.com>
2020-12-04 07:22:58 +0100

    day 4 part 2

--- 2020/src/bin/day-4.rs
+++ 2020/src/bin/day-4.rs
@@ -41,6 +41,7 @@ fn main() {
     }
 
     println!("part 1: {}", part_1(&passports));
+    println!("part 2: {}", part_2(&passports));
 }
 
 fn part_1(passports: &[Passport]) -> usize {
@@ -53,6 +54,96 @@ fn part_1(passports: &[Passport]) -> usize {
             && passport.hair_color.is_some()
             && passport.eye_color.is_some()
             && passport.passport_id.is_some()) as usize;
+    }
+    valid
+}
+
+fn part_2(passports: &[Passport]) -> usize {
+    let mut valid = 0;
+    for passport in passports.iter() {
+        // :=
+        if let Some(birth_year) = passport.birth_year {
+            if birth_year < 1920 || birth_year > 2002 {
+                continue;
+            }
+        } else {
+            continue;
+        }
+        if let Some(issue_year) = passport.issue_year {
+            if issue_year < 2010 || issue_year > 2020 {
+                continue;
+            }
+        } else {
+            continue;
+        }
+        if let Some(expiration_year) = passport.expiration_year {
+            if expiration_year < 2020 || expiration_year > 2030 {
+                continue;
+            }
+        } else {
+            continue;
+        }
+        if let Some(height) = &passport.height {
+            if height.ends_with("cm") {
+                if let Ok(height) = height[..(height.len() - 2)].parse::<u32>() {
+                    if height < 150 || height > 193 {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+            } else if height.ends_with("in") {
+                if let Ok(height) = height[..(height.len() - 2)].parse::<u32>() {
+                    if height < 59 || height > 76 {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+            } else {
+                continue;
+            }
+        } else {
+            continue;
+        }
+        if let Some(hair_color) = &passport.hair_color {
+            if hair_color.len() != 7 {
+                continue;
+            }
+            let mut it = hair_color.chars();
+            if it.next().unwrap() != '#' {
+                continue;
+            }
+            let mut ok = true;
+            while let Some(digit) = it.next() {
+                if !((digit >= '0' && digit <= '9') || (digit >= 'a' && digit <= 'f')) {
+                    ok = false;
+                    break;
+                }
+            }
+            if !ok {
+                continue;
+            }
+        } else {
+            continue;
+        }
+        if let Some(eye_color) = &passport.eye_color {
+            let eye_color: &str = &eye_color;
+            match eye_color {
+                "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => (),
+                _ => continue,
+            }
+        } else {
+            continue;
+        }
+        if let Some(passport_id) = &passport.passport_id {
+            if passport_id.len() != 9 || !passport_id.parse::<u32>().is_ok() {
+                continue;
+            }
+        } else {
+            continue;
+        }     
+        valid += 1;
     }
     valid
 }
