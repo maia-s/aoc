247c33472127b42ee1d1810f2dc73a936827f6eb
Maia <66437537+maia-s@users.noreply.github.com>
2020-12-13 09:39:45 +0100

    day 13 part 2 (slow)

--- 2020/src/bin/day-13.rs
+++ 2020/src/bin/day-13.rs
@@ -8,19 +8,37 @@ fn main() {
         .unwrap()
         .split(',')
         .filter_map(|x| match x {
-            "x" => None,
-            n => Some(n.parse().unwrap()),
+            "x" => Some(None),
+            n => Some(Some(n.parse().unwrap())),
         })
         .collect();
 
     println!("part 1: {}", part_1(earliest, &buses));
+    println!("part 2: {}", part_2(&buses));
 }
 
-fn part_1(earliest: usize, buses: &[usize]) -> usize {
+fn part_1(earliest: usize, buses: &[Option<usize>]) -> usize {
     let min = buses
         .iter()
-        .map(|&bus| (bus - earliest % bus, bus))
+        .filter_map(|bus| bus.map(|bus| (bus - earliest % bus, bus)))
         .min()
         .unwrap();
     min.0 * min.1
+}
+
+fn part_2(buses: &[Option<usize>]) -> usize {
+    let mut buses: Vec<_> = buses
+        .iter()
+        .enumerate()
+        .filter_map(|(i, bus)| bus.map(|bus| (i, bus)))
+        .collect();
+    buses.sort_unstable_by(|a, b| a.1.cmp(&b.1).reverse());
+
+    for i in ((buses[0].1 - buses[0].0)..).step_by(buses[0].1) {
+        if buses.iter().all(|(j, bus)| (i + j) % bus == 0) {
+            return i;
+        }
+    }
+
+    unreachable!();
 }
