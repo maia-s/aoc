31a535b32507020abc45aa376d2587d56a26c384
Maia <66437537+maia-s@users.noreply.github.com>
2021-12-02 11:20:56 +0100

    day 2 part 2

--- 2021/src/bin/day-2.rs
+++ 2021/src/bin/day-2.rs
@@ -3,9 +3,9 @@ use std::str::FromStr;
 const INPUT: &str = include_str!("day-2.input");
 
 enum Command {
-    Forward(usize),
-    Down(usize),
-    Up(usize),
+    Forward(isize),
+    Down(isize),
+    Up(isize),
 }
 
 impl FromStr for Command {
@@ -14,7 +14,7 @@ impl FromStr for Command {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut s = s.split_ascii_whitespace();
         let cmd = s.next().unwrap();
-        let n = s.next().unwrap().parse::<usize>().unwrap();
+        let n = s.next().unwrap().parse::<isize>().unwrap();
         Ok(match cmd {
             "forward" => Command::Forward(n),
             "down" => Command::Down(n),
@@ -25,7 +25,10 @@ impl FromStr for Command {
 }
 
 fn main() {
-    let input: Vec<_> = INPUT.lines().map(|s| s.parse::<Command>().unwrap()).collect();
+    let input: Vec<_> = INPUT
+        .lines()
+        .map(|s| s.parse::<Command>().unwrap())
+        .collect();
 
     // part 1
     let mut xpos = 0;
@@ -38,4 +41,20 @@ fn main() {
         }
     }
     println!("part 1: {}", xpos * depth);
+
+    // part 2
+    let mut xpos = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+    for cmd in input.iter() {
+        match cmd {
+            Command::Forward(n) => {
+                xpos += n;
+                depth += aim * n;
+            }
+            Command::Down(n) => aim += n,
+            Command::Up(n) => aim -= n,
+        }
+    }
+    println!("part 2: {}", xpos * depth);
 }
