0a884cbdcb37dc2605d57998b1b94bbd85e7ac60
Maia <66437537+maia-s@users.noreply.github.com>
2021-12-03 12:43:27 +0100

    part 2

--- 2021/src/bin/day-3.rs
+++ 2021/src/bin/day-3.rs
@@ -3,28 +3,56 @@ const INPUT: &str = include_str!("day-3.input");
 fn main() {
     let input: Vec<Vec<_>> = INPUT
         .lines()
-        .map(|s| s.chars().map(|c| c as usize - b'0' as usize).collect())
+        .map(|s| s.chars().map(|c| c as usize - b'0' as usize != 0).collect())
         .collect();
-    let digits = input[0].len();
-    let n = input.len() as f64;
+    let n = input.len();
 
-    let sums = input.iter().fold(vec![0; digits], |mut sum, v| {
-        for (s, i) in sum.iter_mut().zip(v.iter()) {
-            *s += i;
+    // part 1
+    let sums = sums(&input);
+    let gamma = num(&gammas(&sums, n));
+    let epsilon = num(&epsilons(&sums, n));
+    println!("part 1: {}", gamma * epsilon);
+
+    // part 2
+    let oxygen = part2(input.clone(), true);
+    let scrubber = part2(input, false);
+    println!("part 2: {}", oxygen * scrubber);
+}
+
+fn part2(mut input: Vec<Vec<bool>>, criteria: bool) -> usize {
+    let mut i = 0;
+    while input.len() > 1 {
+        let n = input.len();
+        let sums = sums(&input);
+        let keep = if criteria {
+            gammas(&sums, n)
+        } else {
+            epsilons(&sums, n)
+        };
+        input = input.into_iter().filter(|v| v[i] == keep[i]).collect();
+        i += 1;
+    }
+    num(&input[0])
+}
+
+fn sums(input: &[Vec<bool>]) -> Vec<usize> {
+    let digits = input[0].len();
+    input.iter().fold(vec![0; digits], |mut sum, v| {
+        for (s, &i) in sum.iter_mut().zip(v.iter()) {
+            *s += i as usize;
         }
         sum
-    });
+    })
+}
 
-    let gamma = num(&sums
-        .iter()
-        .map(|&i| i as f64 / n >= 0.5)
-        .collect::<Vec<bool>>());
-    let epsilon = num(&sums
-        .iter()
-        .map(|&i| i as f64 / n < 0.5)
-        .collect::<Vec<bool>>());
+fn gammas(sums: &[usize], n: usize) -> Vec<bool> {
+    let n = n as f64;
+    sums.iter().map(|&i| i as f64 / n >= 0.5).collect()
+}
 
-    println!("part 1: {}", gamma * epsilon);
+fn epsilons(sums: &[usize], n: usize) -> Vec<bool> {
+    let n = n as f64;
+    sums.iter().map(|&i| i as f64 / n < 0.5).collect()
 }
 
 fn num(v: &[bool]) -> usize {
