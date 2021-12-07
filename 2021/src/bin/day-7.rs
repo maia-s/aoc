433799a64c12244d4108d27fcbcc4c9c0a150165
Maia <66437537+maia-s@users.noreply.github.com>
2021-12-07 11:19:32 +0100

    day 7 part 2

--- 2021/src/bin/day-7.rs
+++ 2021/src/bin/day-7.rs
@@ -8,7 +8,6 @@ fn main() {
         .collect();
 
     let mut i = input.iter().sum::<usize>() / input.len();
-    let mut min_i = i;
     let mut min_cost = cost(&input, i);
     let d = if i > 0 { -1 } else { 1 };
 
@@ -17,17 +16,41 @@ fn main() {
         let cost = cost(&input, i);
         if min_cost > cost {
             min_cost = cost;
-            min_i = i;
         } else {
             break;
         }
     }
     println!("part 1: {}", min_cost);
+
+    let mut i = input.iter().sum::<usize>() / input.len();
+    let mut min_cost = cost_2(&input, i);
+    let d = if i > 0 { -1 } else { 1 };
+
+    while (0..input.len()).contains(&((i as isize + d) as usize)) {
+        i = (i as isize + d) as usize;
+        let cost = cost_2(&input, i);
+        if min_cost > cost {
+            min_cost = cost;
+        } else {
+            break;
+        }
+    }
+    println!("part 2: {}", min_cost);
 }
 
 fn cost(crabs: &[usize], pos: usize) -> usize {
     crabs
         .iter()
         .map(|&x| if x > pos { x - pos } else { pos - x })
+        .sum()
+}
+
+fn cost_2(crabs: &[usize], pos: usize) -> usize {
+    crabs
+        .iter()
+        .map(|&x| {
+            let x = if x > pos { x - pos } else { pos - x };
+            x * (x + 1) / 2
+        })
         .sum()
 }
