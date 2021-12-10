4074ffab814d15877853a2d40d54762645a1dff3
Maia <66437537+maia-s@users.noreply.github.com>
2021-12-10 11:26:29 +0100

    day 10 part 2

--- 2021/src/bin/day-10.rs
+++ 2021/src/bin/day-10.rs
@@ -2,9 +2,11 @@ const INPUT: &str = include_str!("day-10.input");
 
 fn main() {
     let mut part_1 = 0_usize;
+    let mut part_2 = Vec::new();
+    let mut stack = Vec::new();
 
     for line in INPUT.lines() {
-        let mut stack = Vec::new();
+        stack.clear();
         for c in line.trim().chars() {
             let (expect, points) = match c {
                 '(' | '[' | '{' | '<' => {
@@ -20,11 +22,30 @@ fn main() {
             if let Some(popped) = stack.pop() {
                 if popped != expect {
                     part_1 += points;
+                    stack.clear();
                     break;
                 }
             }
         }
+        if !stack.is_empty() {
+            let mut score = 0_usize;
+            while let Some(popped) = stack.pop() {
+                score = score * 5
+                    + match popped {
+                        '(' => 1,
+                        '[' => 2,
+                        '{' => 3,
+                        '<' => 4,
+                        _ => unreachable!(),
+                    };
+            }
+            part_2.push(score);
+        }
     }
 
     println!("part 1: {}", part_1);
+
+    part_2.sort_unstable();
+
+    println!("part 2: {}", part_2[part_2.len() / 2]);
 }
