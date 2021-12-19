3f4d10ff67530cb51390f8e577a4017e9bfce3d4
Maia <66437537+maia-s@users.noreply.github.com>
2021-12-19 13:52:09 +0100

    orientations

--- 2021/src/bin/day-19.rs
+++ 2021/src/bin/day-19.rs
@@ -30,11 +30,173 @@ impl FromStr for Scanner {
     }
 }
 
+#[derive(Clone, Copy)]
+enum Orientation {
+    X0Y0Z0,
+    X0Y0Z1,
+    X0Y0Z2,
+    X0Y0Z3,
+
+    X0Y1Z0,
+    X0Y1Z1,
+    X0Y1Z2,
+    X0Y1Z3,
+
+    X0Y2Z0,
+    X0Y2Z1,
+    X0Y2Z2,
+    X0Y2Z3,
+
+    X0Y3Z0,
+    X0Y3Z1,
+    X0Y3Z2,
+    X0Y3Z3,
+
+    X1Y0Z0,
+    X1Y0Z1,
+    X1Y0Z2,
+    X1Y0Z3,
+
+    X3Y0Z0,
+    X3Y0Z1,
+    X3Y0Z2,
+    X3Y0Z3,
+}
+
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Point {
     x: i32,
     y: i32,
     z: i32,
+}
+
+impl Point {
+    #[must_use]
+    fn rotate(&self, o: Orientation) -> Point {
+        match o {
+            Orientation::X0Y0Z0 => *self,
+            Orientation::X0Y0Z1 => Point {
+                x: self.y,
+                y: -self.x,
+                z: self.z,
+            },
+            Orientation::X0Y0Z2 => Point {
+                x: -self.x,
+                y: -self.y,
+                z: self.z,
+            },
+            Orientation::X0Y0Z3 => Point {
+                x: -self.y,
+                y: self.x,
+                z: self.z,
+            },
+
+            Orientation::X0Y1Z0 => Point {
+                x: -self.z,
+                y: self.y,
+                z: self.x,
+            },
+            Orientation::X0Y1Z1 => Point {
+                x: self.y,
+                y: self.z,
+                z: self.x,
+            },
+            Orientation::X0Y1Z2 => Point {
+                x: self.z,
+                y: -self.y,
+                z: self.x,
+            },
+            Orientation::X0Y1Z3 => Point {
+                x: -self.y,
+                y: -self.z,
+                z: self.x,
+            },
+
+            Orientation::X0Y2Z0 => Point {
+                x: -self.x,
+                y: self.y,
+                z: -self.z,
+            },
+            Orientation::X0Y2Z1 => Point {
+                x: self.y,
+                y: self.x,
+                z: -self.z,
+            },
+            Orientation::X0Y2Z2 => Point {
+                x: self.x,
+                y: -self.y,
+                z: -self.z,
+            },
+            Orientation::X0Y2Z3 => Point {
+                x: -self.y,
+                y: -self.x,
+                z: -self.z,
+            },
+
+            Orientation::X0Y3Z0 => Point {
+                x: self.z,
+                y: self.y,
+                z: -self.x,
+            },
+            Orientation::X0Y3Z1 => Point {
+                x: self.y,
+                y: -self.z,
+                z: -self.x,
+            },
+            Orientation::X0Y3Z2 => Point {
+                x: -self.z,
+                y: -self.y,
+                z: -self.x,
+            },
+            Orientation::X0Y3Z3 => Point {
+                x: -self.y,
+                y: self.z,
+                z: -self.x,
+            },
+
+            Orientation::X1Y0Z0 => Point {
+                x: self.x,
+                y: -self.z,
+                z: self.y,
+            },
+            Orientation::X1Y0Z1 => Point {
+                x: -self.z,
+                y: -self.x,
+                z: self.y,
+            },
+            Orientation::X1Y0Z2 => Point {
+                x: -self.x,
+                y: self.z,
+                z: self.y,
+            },
+            Orientation::X1Y0Z3 => Point {
+                x: self.z,
+                y: self.x,
+                z: self.y,
+            },
+
+            Orientation::X3Y0Z0 => Point {
+                x: self.x,
+                y: self.z,
+                z: -self.y,
+            },
+            Orientation::X3Y0Z1 => Point {
+                x: -self.z,
+                y: self.x,
+                z: -self.y,
+            },
+            Orientation::X3Y0Z2 => Point {
+                x: -self.x,
+                y: -self.z,
+                z: -self.y,
+            },
+            Orientation::X3Y0Z3 => Point {
+                x: self.z,
+                y: -self.x,
+                z: -self.y,
+            },
+        }
+    }
 }
 
 impl FromStr for Point {
@@ -54,13 +216,7 @@ struct Configuration {
     orientation: Orientation,
 }
 
-impl Configuration {
-    fn map_point(&self, dst: &Configuration, point: Point) -> Point {
-        todo!()
-    }
-}
-
-enum Orientation {}
+impl Configuration {}
 
 fn main() {
     let mut scanners: Vec<Scanner> = INPUT.split("\n\n").map(|s| s.parse().unwrap()).collect();
