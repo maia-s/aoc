a63ad3674f6b14a18f988c72f898e51e68e118e6
Maia <66437537+maia-s@users.noreply.github.com>
2021-12-20 20:54:34 +0100

    :/

--- 2021/src/bin/day-20.rs
+++ 2021/src/bin/day-20.rs
@@ -1,18 +1,32 @@
-use std::{collections::HashSet, fmt::Display, str::FromStr};
+use std::{collections::HashMap, fmt::Display, str::FromStr};
 
 const INPUT: &str = include_str!("day-20.input");
 
+#[derive(Default)]
 struct Image {
-    pixels: HashSet<(isize, isize)>,
+    pixels: HashMap<(isize, isize), bool>,
+    unset: bool,
 }
 
 impl Image {
-    fn len(&self) -> usize {
-        self.pixels.len()
+    fn lit(&self) -> usize {
+        self.pixels.iter().filter(|(_, &set)| set).count()
     }
 
     fn get(&self, x: isize, y: isize) -> bool {
-        self.pixels.contains(&(x, y))
+        *self.pixels.get(&(x, y)).unwrap_or(&self.unset)
+    }
+
+    fn set(&mut self, x: isize, y: isize, value: bool) {
+        self.pixels.insert((x, y), value);
+        self.pixels.entry((x - 1, y - 1)).or_insert(false);
+        self.pixels.entry((x, y - 1)).or_insert(false);
+        self.pixels.entry((x + 1, y - 1)).or_insert(false);
+        self.pixels.entry((x - 1, y)).or_insert(false);
+        self.pixels.entry((x + 1, y)).or_insert(false);
+        self.pixels.entry((x - 1, y + 1)).or_insert(false);
+        self.pixels.entry((x, y + 1)).or_insert(false);
+        self.pixels.entry((x + 1, y + 1)).or_insert(false);
     }
 
     fn chunk(&self, x: isize, y: isize) -> usize {
@@ -29,17 +43,12 @@ impl Image {
     }
 
     fn enhance(&mut self, lut: &[bool]) {
-        self.pixels = self
-            .pixels
-            .iter()
-            .filter_map(|&(x, y)| {
-                if lut[self.chunk(x, y)] {
-                    Some((x, y))
-                } else {
-                    None
-                }
-            })
-            .collect();
+        let mut new = Self::default();
+        for (&(x, y), _) in self.pixels.iter() {
+            new.set(x, y, lut[self.chunk(x, y)]);
+        }
+        new.unset = lut[self.unset as usize * 0x1ff];
+        *self = new;
     }
 }
 
@@ -47,15 +56,13 @@ impl FromStr for Image {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut pixels = HashSet::new();
+        let mut image = Image::default();
         for (y, line) in s.lines().enumerate() {
             for (x, c) in line.chars().enumerate() {
-                if c == '#' {
-                    pixels.insert((x as isize, y as isize));
-                }
+                image.set(x as isize, y as isize, c == '#');
             }
         }
-        Ok(Self { pixels })
+        Ok(image)
     }
 }
 
@@ -63,7 +70,7 @@ impl Display for Image {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let (mut min_x, mut max_x) = (isize::MAX, isize::MIN);
         let (mut min_y, mut max_y) = (isize::MAX, isize::MIN);
-        for &(x, y) in self.pixels.iter() {
+        for (&(x, y), _) in self.pixels.iter() {
             min_x = min_x.min(x);
             max_x = max_x.max(x);
             min_y = min_y.min(y);
@@ -84,7 +91,7 @@ fn main() {
 
     image.enhance(&lut);
     image.enhance(&lut);
-    println!("part 1: {}", image.len());
+    println!("part 1: {}", image.lit());
 }
 
 fn parse(input: &str) -> (Vec<bool>, Image) {
@@ -110,10 +117,12 @@ mod tests {
         let (lut, mut image) = parse(input);
         assert!(lut[34]);
         eprintln!("{}", image);
+        assert_eq!(image.chunk(2, 2), 34);
         image.enhance(&lut);
         eprintln!("{}", image);
+        assert!(image.get(2, 2));
         image.enhance(&lut);
         eprintln!("{}", image);
-        assert_eq!(image.len(), 35);
+        assert_eq!(image.lit(), 35);
     }
 }
