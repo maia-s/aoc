1aaca17b0d59b2edfdfbc17ed0fdda6af532da9f
Maia <66437537+maia-s@users.noreply.github.com>
2021-12-21 13:32:37 +0100

    part 2

--- 2021/src/bin/day-21.rs
+++ 2021/src/bin/day-21.rs
@@ -4,7 +4,7 @@ const INPUT: &str = include_str!("day-21.input");
 
 enum Universe<D: Die, const WIN: u16> {
     Game(Game<D, WIN>),
-    Won(u8, usize),
+    Won(usize, u8, usize),
 }
 
 #[derive(Clone)]
@@ -12,8 +12,8 @@ struct Game<D: Die, const WIN: u16> {
     die: D,
     players: Vec<Player>,
     current_player: u8,
-    current_roll: u16,
     roll_count: u16,
+    weight: usize,
 }
 
 impl<D: Die, const WIN: u16> FromStr for Game<D, WIN> {
@@ -36,8 +36,8 @@ impl<D: Die, const WIN: u16> FromStr for Game<D, WIN> {
             die: D::default(),
             players,
             current_player: 0,
-            current_roll: 0,
             roll_count: 0,
+            weight: 1,
         })
     }
 }
@@ -45,26 +45,22 @@ impl<D: Die, const WIN: u16> FromStr for Game<D, WIN> {
 impl<D: Die, const WIN: u16> Game<D, WIN> {
     fn roll(mut self) -> Vec<Universe<D, WIN>> {
         self.die
-            .roll()
+            .roll_3()
             .into_iter()
-            .map(|roll| {
+            .map(|(weight, roll)| {
                 let mut game = self.clone();
-                game.current_roll += roll as u16;
-                game.roll_count += 1;
-                if game.roll_count % 3 == 0 {
-                    let current_player = game.current_player;
-                    let score = game.players[current_player as usize].forward(game.current_roll);
-                    game.current_player = (game.current_player + 1) % game.players.len() as u8;
-                    game.current_roll = 0;
-                    if score >= WIN {
-                        Universe::Won(
-                            current_player,
-                            game.players[game.current_player as usize].score as usize
-                                * game.roll_count as usize,
-                        )
-                    } else {
-                        Universe::Game(game)
-                    }
+                game.weight *= weight;
+                game.roll_count += 3;
+                let current_player = game.current_player;
+                let score = game.players[current_player as usize].forward(roll);
+                game.current_player = (game.current_player + 1) % game.players.len() as u8;
+                if score >= WIN {
+                    Universe::Won(
+                        game.weight,
+                        current_player,
+                        game.players[game.current_player as usize].score as usize
+                            * game.roll_count as usize,
+                    )
                 } else {
                     Universe::Game(game)
                 }
@@ -80,29 +76,36 @@ struct Player {
 }
 
 impl Player {
-    fn forward(&mut self, n: u16) -> u16 {
-        self.pos = ((self.pos as u16 + n - 1) % 10 + 1) as u8;
-        self.score += self.pos as u16;
+    fn forward(&mut self, n: u32) -> u16 {
+        let pos = (self.pos as u32 + n - 1) % 10 + 1;
+        self.pos = pos as u8;
+        self.score += pos as u16;
         self.score
     }
 }
 
 trait Die: Copy + Default {
-    fn roll(&mut self) -> Vec<u8>;
+    fn roll_3(&mut self) -> Vec<(usize, u32)>;
 }
 
 #[derive(Clone, Copy, Default)]
 struct DeterministicDie(u8);
 
-impl Die for DeterministicDie {
-    fn roll(&mut self) -> Vec<u8> {
+impl DeterministicDie {
+    fn roll(&mut self) -> u32 {
         let roll = self.0 + 1;
         if roll == 100 {
             self.0 = 0;
         } else {
             self.0 = roll;
         }
-        vec![roll]
+        roll as u32
+    }
+}
+
+impl Die for DeterministicDie {
+    fn roll_3(&mut self) -> Vec<(usize, u32)> {
+        vec![(1, self.roll() + self.roll() + self.roll())]
     }
 }
 
@@ -110,8 +113,8 @@ impl Die for DeterministicDie {
 struct DiracDie;
 
 impl Die for DiracDie {
-    fn roll(&mut self) -> Vec<u8> {
-        vec![1, 2, 3]
+    fn roll_3(&mut self) -> Vec<(usize, u32)> {
+        vec![(1, 3), (3, 4), (6, 5), (7, 6), (6, 7), (3, 8), (1, 9)]
     }
 }
 
@@ -130,11 +133,11 @@ fn play<D: Die, const WIN: u16>(input: &str, get_first_win: bool) -> usize {
     while !multiverse.is_empty() {
         match multiverse.pop().unwrap() {
             Universe::Game(game) => multiverse.extend(game.roll()),
-            Universe::Won(player, score) => {
+            Universe::Won(weight, player, score) => {
                 if get_first_win {
                     return score;
                 }
-                wins[player as usize] += 1;
+                wins[player as usize] += weight;
             }
         }
     }
