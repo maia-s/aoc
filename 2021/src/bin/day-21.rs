88de9f65761f096c615687185ee016513d7ad5cd
Maia <66437537+maia-s@users.noreply.github.com>
2021-12-21 12:54:48 +0100

    part 2 (slow)

--- 2021/src/bin/day-21.rs
+++ 2021/src/bin/day-21.rs
@@ -2,13 +2,21 @@ use std::str::FromStr;
 
 const INPUT: &str = include_str!("day-21.input");
 
-struct Game<D: Die> {
-    die: D,
-    players: Vec<Player>,
-    roll_count: usize,
+enum Universe<D: Die, const WIN: u16> {
+    Game(Game<D, WIN>),
+    Won(u8, usize),
 }
 
-impl<D: Die> FromStr for Game<D> {
+#[derive(Clone)]
+struct Game<D: Die, const WIN: u16> {
+    die: D,
+    players: Vec<Player>,
+    current_player: u8,
+    current_roll: u16,
+    roll_count: u16,
+}
+
+impl<D: Die, const WIN: u16> FromStr for Game<D, WIN> {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -23,79 +31,114 @@ impl<D: Die> FromStr for Game<D> {
                 }
             })
             .collect();
+        assert_eq!(players.len(), 2);
         Ok(Self {
             die: D::default(),
             players,
+            current_player: 0,
+            current_roll: 0,
             roll_count: 0,
         })
     }
 }
 
-impl<D: Die> Game<D> {
-    fn play(&mut self) -> usize {
-        loop {
-            if let Some(score) = self.round() {
-                return score;
-            }
-        }
-    }
-
-    fn round(&mut self) -> Option<usize> {
-        assert_eq!(self.players.len(), 2);
-        for (i, p) in self.players.iter_mut().enumerate() {
-            let roll = self.die.roll_n(3);
-            self.roll_count += 3;
-            if p.forward(roll) {
-                return Some(self.players[1 - i].score * self.roll_count);
-            }
-        }
-        None
+impl<D: Die, const WIN: u16> Game<D, WIN> {
+    fn roll(mut self) -> Vec<Universe<D, WIN>> {
+        self.die
+            .roll()
+            .into_iter()
+            .map(|roll| {
+                let mut game = self.clone();
+                game.current_roll += roll as u16;
+                game.roll_count += 1;
+                if game.roll_count % 3 == 0 {
+                    let current_player = game.current_player;
+                    let score = game.players[current_player as usize].forward(game.current_roll);
+                    game.current_player = (game.current_player + 1) % game.players.len() as u8;
+                    game.current_roll = 0;
+                    if score >= WIN {
+                        Universe::Won(
+                            current_player,
+                            game.players[game.current_player as usize].score as usize
+                                * game.roll_count as usize,
+                        )
+                    } else {
+                        Universe::Game(game)
+                    }
+                } else {
+                    Universe::Game(game)
+                }
+            })
+            .collect()
     }
 }
 
+#[derive(Clone, Copy)]
 struct Player {
-    pos: usize,
-    score: usize,
+    pos: u8,
+    score: u16,
 }
 
 impl Player {
-    fn forward(&mut self, n: usize) -> bool {
-        self.pos = (self.pos + n - 1) % 10 + 1;
-        self.score += self.pos;
-        self.score >= 1000
+    fn forward(&mut self, n: u16) -> u16 {
+        self.pos = ((self.pos as u16 + n - 1) % 10 + 1) as u8;
+        self.score += self.pos as u16;
+        self.score
     }
 }
 
-trait Die: Default {
-    fn roll(&mut self) -> usize;
-
-    fn roll_n(&mut self, n: usize) -> usize {
-        let mut sum = 0;
-        for _ in 0..n {
-            sum += self.roll();
-        }
-        sum
-    }
+trait Die: Copy + Default {
+    fn roll(&mut self) -> Vec<u8>;
 }
 
-#[derive(Default)]
-struct DeterministicDie(usize);
+#[derive(Clone, Copy, Default)]
+struct DeterministicDie(u8);
 
 impl Die for DeterministicDie {
-    fn roll(&mut self) -> usize {
+    fn roll(&mut self) -> Vec<u8> {
         let roll = self.0 + 1;
         if roll == 100 {
             self.0 = 0;
         } else {
             self.0 = roll;
         }
-        roll
+        vec![roll]
+    }
+}
+
+#[derive(Clone, Copy, Default)]
+struct DiracDie;
+
+impl Die for DiracDie {
+    fn roll(&mut self) -> Vec<u8> {
+        vec![1, 2, 3]
     }
 }
 
 fn main() {
-    let mut game = INPUT.parse::<Game<DeterministicDie>>().unwrap();
-    println!("part 1: {}", game.play());
+    let part_1 = play::<DeterministicDie, 1000>(INPUT, true);
+    println!("part 1: {}", part_1);
+
+    let part_2 = play::<DiracDie, 21>(INPUT, false);
+    println!("part 2: {}", part_2);
+}
+
+fn play<D: Die, const WIN: u16>(input: &str, get_first_win: bool) -> usize {
+    let start = input.parse::<Game<D, WIN>>().unwrap();
+    let mut wins = vec![0; start.players.len()];
+    let mut multiverse = vec![Universe::Game(start)];
+    while !multiverse.is_empty() {
+        match multiverse.pop().unwrap() {
+            Universe::Game(game) => multiverse.extend(game.roll()),
+            Universe::Won(player, score) => {
+                if get_first_win {
+                    return score;
+                }
+                wins[player as usize] += 1;
+            }
+        }
+    }
+    wins.into_iter().max().unwrap()
 }
 
 #[cfg(test)]
@@ -104,8 +147,7 @@ mod tests {
 
     #[test]
     fn example() {
-        let input = "Player 1 starting position: 4\nPlayer 2 starting position: 8";        
-        let mut game = input.parse::<Game<DeterministicDie>>().unwrap();
-        assert_eq!(game.play(), 739785);
+        let input = "Player 1 starting position: 4\nPlayer 2 starting position: 8";
+        assert_eq!(play::<DeterministicDie, 1000>(input, true), 739785);
     }
 }
