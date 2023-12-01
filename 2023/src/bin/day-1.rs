63f958322b6b5092645d444a4be61277d93c6608
Maia <66437537+maia-s@users.noreply.github.com>
2023-12-01 17:49:19 +0100

    day 1 part 2

--- 2023/src/bin/day-1.rs
+++ 2023/src/bin/day-1.rs
@@ -1,24 +1,61 @@
 const INPUT: &str = include_str!("day-1.txt");
+
+#[allow(dead_code)]
 const INPUT_EX: &str = "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet";
 
+#[allow(dead_code)]
+const INPUT_EX2: &str = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+
 aoc_2023::aoc! {
     struct Day1 {
         sum: usize,
+        sum2: usize,
     }
 
     self(input) {
+        const WORDS: &[&str] = &["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
         let mut sum = 0;
+        let mut sum2 = 0;
+
         for line in input.lines() {
-            let nums: Vec<usize> = line.trim().chars().filter_map(|c| match c {
+            let line = line.trim();
+
+            let nums: Vec<usize> = line.chars().filter_map(|c| match c {
                 '0'..='9' => Some(c as usize - b'0' as usize),
                 _ => None
             }).collect();
-            sum += nums.first().unwrap() * 10 + nums.last().unwrap();
+
+            let mut nums2 = Vec::new();
+            'lines: for i in 0..line.len() {
+                let rest = &line[i..];
+                nums2.push('num: {
+                    match rest.chars().next().unwrap() {
+                        c @ '0'..='9' => break 'num c as usize - b'0' as usize,
+                        _ => {
+                            for (wi, word) in WORDS.iter().enumerate() {
+                                if rest.starts_with(word) {
+                                    break 'num wi + 1;
+                                }
+                            }
+                            continue 'lines;
+                        }
+                    }
+                });
+            }
+
+            sum += nums.first().unwrap_or(&0) * 10 + nums.last().unwrap_or(&0);
+            sum2 += nums2.first().unwrap_or(&0) * 10 + nums2.last().unwrap_or(&0);
         }
-        Ok(Day1 { sum })
+        Ok(Day1 { sum, sum2 })
     }
 
     part1 usize {
@@ -26,10 +63,12 @@ aoc_2023::aoc! {
     }
 
     part2 usize {
-        Ok(0)
+        Ok(self.sum2)
     }
 
     input = INPUT;
 
-    test day1_example(INPUT_EX, 142, 0);
+    test day1_example(INPUT_EX, 142, 142);
+    test day1_example2(INPUT_EX2, 209, 281);
+    test day1(INPUT, 55712, 55413);
 }
