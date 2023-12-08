c6ae895c8432e561b57b209c8427cb0192f5f3c0
Maia <66437537+maia-s@users.noreply.github.com>
2023-12-08 19:40:09 +0100

    part 2 (35s)

--- 2023/src/bin/day-8.rs
+++ 2023/src/bin/day-8.rs
@@ -70,26 +70,42 @@ aoc! {
     }
 
     part2 usize {
+        // so not stated in the puzzle description but apparently something we can rely on
+        // is that each start only has one possible end and repeats after that with the same
+        // cycle length each time
         let step = self.path.iter().copied().cycle();
-        let mut nodes = self.map.keys().copied().filter(|key| key.ends_with('A'))
-            .map(|start| Node::new(start, step.clone())).collect::<Vec<_>>();
-        let mut steps = 0;
-        'next: loop {
-            for node in nodes.iter_mut() {
-                let s = node.run(&self.map, |s, n| s >= steps && n.ends_with('Z'))?;
-                if s > steps {
-                    steps = s;
-                    continue 'next;
+        let cycles = self.map.keys().copied().filter(|key| key.ends_with('A'))
+            .map(|start| {
+                let mut node = start;
+                step.clone().enumerate().find_map(|(i, step)| {
+                    node = self.map.get(node).expect("missing node")[step as usize];
+                    node.ends_with('Z').then_some(i + 1)
+                }).expect("no end")
+            }).collect::<Vec<_>>();
+        let mut lcm = cycles.clone();
+        let lcm = loop {
+            let mut min = usize::MAX;
+            let mut min_i = 0;
+            let mut max = 0;
+            for (i, &n) in lcm.iter().enumerate() {
+                if min > n {
+                    min = n;
+                    min_i = i;
                 }
+                max = max.max(n);
             }
-            return Ok(steps)
-        }
+            if min == max {
+                break min;
+            }
+            lcm[min_i] += cycles[min_i];
+        };
+        Ok(lcm)
     }
 
     test day8_example(INPUT_EX, 2);
     test day8_example2(INPUT_EX2, 6);
     test day8_example3(INPUT_EX3,, 6);
-    test day8(INPUT, 18113);
+    test day8(INPUT, 18113, 12315788159977);
 }
 
 impl Day8<'_> {
@@ -116,35 +132,4 @@ impl Day8<'_> {
 enum Dir {
     Left = 0,
     Right = 1,
-}
-
-struct Node<'a, I: Iterator<Item = Dir>> {
-    current: &'a str,
-    step: I,
-    steps_taken: usize,
-}
-
-impl<'a, I: Iterator<Item = Dir>> Node<'a, I> {
-    fn new(start: &'a str, step: I) -> Self {
-        Self {
-            current: start,
-            step,
-            steps_taken: 0,
-        }
-    }
-
-    fn run(
-        &mut self,
-        map: &HashMap<&'a str, [&'a str; 2]>,
-        stop: impl Fn(usize, &str) -> bool,
-    ) -> Result<usize, Error> {
-        while !stop(self.steps_taken, self.current) {
-            self.steps_taken += 1;
-            self.current = map
-                .get(&self.current)
-                .ok_or_else(|| format!("missing node {}", self.current))?
-                [self.step.next().unwrap() as usize];
-        }
-        Ok(self.steps_taken)
-    }
 }
