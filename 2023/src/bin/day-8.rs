cc69dfa1da035a6417738601498122e5636962e9
Maia <66437537+maia-s@users.noreply.github.com>
2023-12-08 16:38:43 +0100

    day 8 part 1

---
+++ 2023/src/bin/day-8.rs
@@ -0,0 +1,88 @@
+use std::collections::HashMap;
+
+use aoc_2023::{aoc, str_block, Error};
+
+const INPUT: &str = include_str!("day-8.txt");
+
+#[allow(dead_code)]
+const INPUT_EX: &str = str_block! {"
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)"};
+
+#[allow(dead_code)]
+const INPUT_EX2: &str = str_block! {"
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)"};
+
+aoc! {
+    struct Day8<'a> {
+        path: Vec<Dir>,
+        map: HashMap<&'a str, [&'a str; 2]>,
+    }
+
+    self(input = INPUT) {
+        let mut input = input.lines();
+
+        let path = input.next().ok_or("missing directions")?;
+        let path = path.chars().map(|c| match c {
+            'L' => Ok(Dir::Left),
+            'R' => Ok(Dir::Right),
+            _ => Err(Error::from("unknown direction")),
+        }).collect::<Result<_, _>>()?;
+
+        input.next();
+
+        let map = input.map(|line| {
+            let (node, paths) = line.split_once(" = (").ok_or("missing ` = (`")?;
+            let (left, right) = paths.split_once(", ").ok_or("missing `, `")?;
+            let right = right.trim_end_matches(')');
+            Ok((node, [left, right]))
+        }).collect::<Result<_, Error>>()?;
+
+        Ok(Self { path, map })
+    }
+
+    part1 usize {
+        Ok(self.steps("AAA", "ZZZ", self.path.iter().copied().cycle())?)
+    }
+
+    test day8_example(INPUT_EX, 2);
+    test day8_example2(INPUT_EX2, 6);
+    test day8(INPUT, 18113);
+}
+
+impl Day8<'_> {
+    fn steps(
+        &self,
+        src: &str,
+        dest: &str,
+        mut step: impl Iterator<Item = Dir>,
+    ) -> Result<usize, Error> {
+        let mut node = src;
+        let mut steps = 0;
+        while node != dest {
+            steps += 1;
+            node = self
+                .map
+                .get(&node)
+                .ok_or(format!("missing node `{node}`"))?[step.next().unwrap() as usize];
+        }
+        Ok(steps)
+    }
+}
+
+#[derive(Clone, Copy)]
+enum Dir {
+    Left = 0,
+    Right = 1,
+}
