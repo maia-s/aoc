b6717178e9128d80b037e0bc975ab0b1e8adcf76
Maia <66437537+maia-s@users.noreply.github.com>
2023-12-11 08:42:04 +0100

    remove unnecessary map struct

--- 2023/src/bin/day-11.rs
+++ 2023/src/bin/day-11.rs
@@ -1,5 +1,3 @@
-use std::ops::Index;
-
 use aoc_2023::{aoc, str_block};
 
 const INPUT: &str = include_str!("day-11.txt");
@@ -27,20 +25,21 @@ aoc! {
 
     self(input = INPUT) {
         let mut galaxies = Vec::new();
-        let map = Map(input.lines().enumerate().map(
+        let mut exp_rows = Vec::new();
+        let map: Vec<&[u8]> = input.lines().enumerate().map(
             |(y, line)| {
-                let line = line.as_bytes().to_owned();
+                let line = line.as_bytes();
+                if line.iter().all(|&c| c == b'.') {
+                    exp_rows.push(y);
+                }
                 for x in line.iter().enumerate().filter(|(_, &b)| b == b'#').map(|(x, _)| x) {
                     galaxies.push((x, y));
                 }
                 line
             }
-        ).collect());
-        let exp_rows = map.rows().enumerate().filter_map(
-            |(i, row)| row.iter().all(|&c| c == b'.').then_some(i)
         ).collect();
-        let exp_cols = map.cols().enumerate().filter_map(
-            |(i, col)| col.iter().all(|&c| c == b'.').then_some(i)
+        let exp_cols = (0..map[0].len()).filter(|&col|
+            map.iter().map(|row| row[col]).all(|c| c == b'.')
         ).collect();
         Ok(Self {
             galaxies,
@@ -88,70 +87,5 @@ impl Day11 {
             }
         }
         d
-    }
-}
-
-struct Map(Vec<Vec<u8>>);
-
-impl Index<(usize, usize)> for Map {
-    type Output = u8;
-
-    fn index(&self, index: (usize, usize)) -> &Self::Output {
-        &self.0[index.1][index.0]
-    }
-}
-
-impl Map {
-    fn rows(&self) -> impl Iterator<Item = &Vec<u8>> {
-        self.0.iter()
-    }
-
-    fn cols(&self) -> impl Iterator<Item = Cols> {
-        let mut col = 0;
-        let col_len = self.0[0].len();
-        std::iter::from_fn(move || {
-            if col < col_len {
-                let item = Cols { map: self, col };
-                col += 1;
-                Some(item)
-            } else {
-                None
-            }
-        })
-    }
-}
-
-struct Cols<'a> {
-    map: &'a Map,
-    col: usize,
-}
-
-impl<'a> Cols<'a> {
-    fn iter(&self) -> impl Iterator<Item = &'a u8> {
-        Col {
-            map: self.map,
-            col: self.col,
-            row: 0,
-        }
-    }
-}
-
-struct Col<'a> {
-    map: &'a Map,
-    col: usize,
-    row: usize,
-}
-
-impl<'a> Iterator for Col<'a> {
-    type Item = &'a u8;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.row < self.map.0.len() {
-            let item = &self.map[(self.col, self.row)];
-            self.row += 1;
-            Some(item)
-        } else {
-            None
-        }
     }
 }
