f696972ad9584451dee7c98495faa344b647aa03
Maia <66437537+maia-s@users.noreply.github.com>
2023-12-11 06:59:09 +0100

    day 11 part 1

---
+++ 2023/src/bin/day-11.rs
@@ -0,0 +1,148 @@
+use std::{collections::HashSet, ops::Index};
+
+use aoc_2023::{aoc, str_block};
+
+const INPUT: &str = include_str!("day-11.txt");
+
+#[allow(dead_code)]
+const INPUT_EX: &str = str_block! {"
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+"};
+
+aoc! {
+    struct Day11 {
+        map: Map,
+        galaxies: Vec<(usize, usize)>,
+        exp_rows: HashSet<usize>,
+        exp_cols: HashSet<usize>,
+    }
+
+    self(input = INPUT) {
+        let mut galaxies = Vec::new();
+        let map = Map(input.lines().enumerate().map(
+            |(y, line)| {
+                let line = line.as_bytes().to_owned();
+                for x in line.iter().enumerate().filter(|(_, &b)| b == b'#').map(|(x, _)| x) {
+                    galaxies.push((x, y));
+                }
+                line
+            }
+        ).collect());
+        let exp_rows = map.rows().enumerate()
+            .filter_map(|(i, row)| row.iter().all(|&c| c == b'.').then_some(i)).collect();
+        let exp_cols = map.cols().enumerate()
+            .filter_map(|(i, col)| col.iter().all(|&c| c == b'.').then_some(i)).collect();
+        Ok(Self {
+            map,
+            galaxies,
+            exp_rows,
+            exp_cols,
+        })
+    }
+
+    part1 usize {
+        Ok((0..self.galaxies.len() - 1).map(
+            |i| (i + 1..self.galaxies.len()).map(
+                |j| self.dist(self.galaxies[i], self.galaxies[j])
+            ).sum::<usize>()
+        ).sum())
+    }
+
+    test day11_example(INPUT_EX, 374);
+}
+
+impl Day11 {
+    fn dist(&self, (ax, ay): (usize, usize), (bx, by): (usize, usize)) -> usize {
+        let (ax, bx) = (ax.min(bx), ax.max(bx));
+        let mut dx = bx - ax;
+        for ix in ax..bx {
+            if self.exp_cols.contains(&ix) {
+                dx += 1;
+            }
+        }
+
+        let (ay, by) = (ay.min(by), ay.max(by));
+        let mut dy = by - ay;
+        for iy in ay..by {
+            if self.exp_rows.contains(&iy) {
+                dy += 1;
+            }
+        }
+
+        dx + dy
+    }
+}
+
+struct Map(Vec<Vec<u8>>);
+
+impl Index<(usize, usize)> for Map {
+    type Output = u8;
+
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        &self.0[index.1][index.0]
+    }
+}
+
+impl Map {
+    fn rows(&self) -> impl Iterator<Item = &Vec<u8>> {
+        self.0.iter()
+    }
+
+    fn cols(&self) -> impl Iterator<Item = Cols> {
+        let mut col = 0;
+        let col_len = self.0[0].len();
+        std::iter::from_fn(move || {
+            if col < col_len {
+                let item = Cols { map: self, col };
+                col += 1;
+                Some(item)
+            } else {
+                None
+            }
+        })
+    }
+}
+
+struct Cols<'a> {
+    map: &'a Map,
+    col: usize,
+}
+
+impl<'a> Cols<'a> {
+    fn iter(&self) -> impl Iterator<Item = &'a u8> {
+        Col {
+            map: self.map,
+            col: self.col,
+            row: 0,
+        }
+    }
+}
+
+struct Col<'a> {
+    map: &'a Map,
+    col: usize,
+    row: usize,
+}
+
+impl<'a> Iterator for Col<'a> {
+    type Item = &'a u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.row < self.map.0.len() {
+            let item = &self.map[(self.col, self.row)];
+            self.row += 1;
+            Some(item)
+        } else {
+            None
+        }
+    }
+}
