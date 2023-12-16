a85202bcea28faf5ab24e6df78214265362070cc
Maia <66437537+maia-s@users.noreply.github.com>
2023-12-16 21:39:17 +0100

    14 part 2 (DNF)

--- 2023/src/bin/day-14.rs
+++ 2023/src/bin/day-14.rs
@@ -26,26 +26,82 @@ aoc! {
     }
 
     part1 usize {
-        let width = self.map[0].len();
-        let height = self.map.len();
+        self.tilt_north();
+        Ok(self.support_beam_weight())
+    }
+
+    part2 usize {
+        for _ in 0..1_000_000_000 {
+            self.tilt_north();
+            self.tilt_west();
+            self.tilt_south();
+            self.tilt_east();
+        }
+        Ok(self.support_beam_weight())
+    }
+
+    test day14_example(INPUT_EX, 136, 64);
+    test day14(INPUT, 109755);
+}
+
+impl Day14 {
+    fn width(&self) -> usize {
+        self.map[0].len()
+    }
+
+    fn height(&self) -> usize {
+        self.map.len()
+    }
+
+    fn tilt(
+        &mut self,
+        xr: impl Clone + Iterator<Item = usize>,
+        yr: impl Clone + Iterator<Item = usize>,
+        dx: isize,
+        dy: isize,
+    ) {
         let mut moved = true;
         while moved {
             moved = false;
-            for y in 0..height - 1 {
-                for x in 0..width {
-                    if self.map[y][x] == b'.' && self.map[y + 1][x] == b'O' {
+            for y in yr.clone() {
+                let y1 = (y as isize + dy) as usize;
+                for x in xr.clone() {
+                    let x1 = (x as isize + dx) as usize;
+                    if self.map[y][x] == b'.' && self.map[y1][x1] == b'O' {
                         moved = true;
                         self.map[y][x] = b'O';
-                        self.map[y + 1][x] = b'.';
+                        self.map[y1][x1] = b'.';
                     }
                 }
             }
         }
-        Ok(self.map.iter().enumerate().map(
-            |(y, row)| row.iter().filter_map(|&c| (c == b'O').then_some(height - y)).sum::<usize>()
-        ).sum())
     }
 
-    test day14_example(INPUT_EX, 136);
-    test day14(INPUT,);
+    fn tilt_north(&mut self) {
+        self.tilt(0..self.width(), 0..self.height() - 1, 0, 1);
+    }
+
+    fn tilt_west(&mut self) {
+        self.tilt(0..self.width() - 1, 0..self.height(), 1, 0);
+    }
+
+    fn tilt_south(&mut self) {
+        self.tilt(0..self.width(), (1..self.height()).rev(), 0, -1);
+    }
+
+    fn tilt_east(&mut self) {
+        self.tilt((1..self.width()).rev(), 0..self.height(), -1, 0);
+    }
+
+    fn support_beam_weight(&self) -> usize {
+        self.map
+            .iter()
+            .enumerate()
+            .map(|(y, row)| {
+                row.iter()
+                    .filter_map(|&c| (c == b'O').then_some(self.height() - y))
+                    .sum::<usize>()
+            })
+            .sum()
+    }
 }
