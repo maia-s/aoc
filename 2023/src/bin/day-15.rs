9832a96f4ad83b2320fa5f6f448efd2f896a4f21
Maia <66437537+maia-s@users.noreply.github.com>
2023-12-16 01:56:23 +0100

    part 2 (doesn't work)

--- 2023/src/bin/day-15.rs
+++ 2023/src/bin/day-15.rs
@@ -1,3 +1,5 @@
+use std::array;
+
 use aoc_2023::aoc;
 
 const INPUT: &str = include_str!("day-15.txt");
@@ -15,14 +17,51 @@ aoc! {
     }
 
     part1 usize {
-        Ok(self.words.iter().map(|s| hash(s.as_bytes()) as usize).sum())
+        Ok(self.words.iter().copied().map(hash).sum())
     }
 
-    test day15_example(INPUT_EX, 1320);
+    part2 usize {
+        let mut boxes: [_; 256] = array::from_fn(|_| Vec::<(&str, u8)>::new());
+
+        for s in self.words.iter() {
+            let hash = hash(s);
+            let bx = &mut boxes[hash];
+            if let Some(lens) = s.strip_suffix('-') {
+                if let Some(i) = find_lens(bx, lens) {
+                    eprintln!("remove {lens} from box {hash} at {i}");
+                    bx.remove(i);
+                } else {
+                    eprintln!("remove {lens} (ignored)");
+                }
+            } else {
+                let (lens, focal) = s.split_once('=').ok_or_else(|| format!("couldn't split {s}"))?;
+                let flen: u8 = focal.parse().map_err(|_| "parse error")?;
+                if let Some(i) = find_lens(bx, lens) {
+                    eprintln!("replace {lens} at box {hash}/{i} with focal length {flen}");
+                    bx[i].1 = flen;
+                } else {
+                    eprintln!("add {lens} to box {hash}/{} with focal length {flen}", bx.len());
+                    bx.push((lens, flen));
+                }
+            }
+        }
+
+        Ok(boxes.iter().enumerate().map(|(i, bx)| {
+            let i = i + 1;
+            bx.iter().enumerate().map(|(j, (_, flen))| i * (j + 1) * *flen as usize).sum::<usize>()
+        }).sum())
+    }
+
+    test day15_example(INPUT_EX, 1320, 145);
     test day15(INPUT, 517315);
 }
 
-fn hash(s: &[u8]) -> u8 {
-    s.iter()
-        .fold(0, |hash, &b| hash.wrapping_add(b).wrapping_mul(17))
+fn hash(s: &str) -> usize {
+    s.as_bytes()
+        .iter()
+        .fold(0_u8, |hash, &b| hash.wrapping_add(b).wrapping_mul(17)) as usize
+}
+
+fn find_lens(v: &[(&str, u8)], s: &str) -> Option<usize> {
+    v.iter().position(|(s2, _)| &s == s2)
 }
