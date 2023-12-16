78ce4a1ec9afcc231815c86913e955261edb3b70
Maia <66437537+maia-s@users.noreply.github.com>
2023-12-16 07:41:18 +0100

    day 16 part 2

--- 2023/src/bin/day-16.rs
+++ 2023/src/bin/day-16.rs
@@ -1,4 +1,4 @@
-use aoc_2023::{aoc, str_block};
+use aoc_2023::{aoc, str_block, Error};
 
 const INPUT: &str = include_str!("day-16.txt");
 
@@ -26,6 +26,44 @@ aoc! {
     }
 
     part1 usize {
+        Ok(self.trace_beam(0, 0, Dir::E)?)
+    }
+
+    part2 usize {
+        let mut max = 0;
+        for y in 0..self.height() {
+            for x in 0..self.width() {
+                if x == 0 {
+                    max = max.max(self.trace_beam(x, y, Dir::E)?);
+                }
+                if x == self.width() - 1 {
+                    max = max.max(self.trace_beam(x, y, Dir::W)?);
+                }
+                if y == 0 {
+                    max = max.max(self.trace_beam(x, y, Dir::S)?);
+                }
+                if y == self.height() - 1 {
+                    max = max.max(self.trace_beam(x, y, Dir::N)?);
+                }
+            }
+        }
+        Ok(max)
+    }
+
+    test day16_example(INPUT_EX, 46, 51);
+    test day16(INPUT, 7543);
+}
+
+impl Day16 {
+    fn width(&self) -> usize {
+        self.map[0].len()
+    }
+
+    fn height(&self) -> usize {
+        self.map.len()
+    }
+
+    fn trace_beam(&self, x: usize, y: usize, dir: Dir) -> Result<usize, Error> {
         let mut bmap = self.map.clone();
         for row in bmap.iter_mut() {
             for c in row.iter_mut() {
@@ -33,7 +71,7 @@ aoc! {
             }
         }
 
-        let mut beams = vec![((0, 0), Dir::E)];
+        let mut beams = vec![((x, y), dir)];
         let mut energized = 0;
 
         while let Some(((x, y), dir)) = beams.pop() {
@@ -65,17 +103,21 @@ aoc! {
                         Dir::W => Dir::N,
                         _ => unreachable!(),
                     }),
-                    b'|' => if matches!(dir, Dir::E | Dir::W) {
-                        push(Dir::N);
-                        push(Dir::S);
-                    } else {
-                        push(dir);
+                    b'|' => {
+                        if matches!(dir, Dir::E | Dir::W) {
+                            push(Dir::N);
+                            push(Dir::S);
+                        } else {
+                            push(dir);
+                        }
                     }
-                    b'-' => if matches!(dir, Dir::N | Dir::S) {
-                        push(Dir::E);
-                        push(Dir::W);
-                    } else {
-                        push(dir);
+                    b'-' => {
+                        if matches!(dir, Dir::N | Dir::S) {
+                            push(Dir::E);
+                            push(Dir::W);
+                        } else {
+                            push(dir);
+                        }
                     }
                     _ => panic!("unknown tile"),
                 }
@@ -83,18 +125,6 @@ aoc! {
         }
 
         Ok(energized)
-    }
-
-    test day16_example(INPUT_EX, 46);
-}
-
-impl Day16 {
-    fn width(&self) -> usize {
-        self.map[0].len()
-    }
-
-    fn height(&self) -> usize {
-        self.map.len()
     }
 }
 
