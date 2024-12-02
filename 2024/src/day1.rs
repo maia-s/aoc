3ea82540893c0239d3905c23be11b0cc4819b0f8
Maia <66437537+maia-s@users.noreply.github.com>
2024-12-02 15:53:22 +0100

    simplify

--- 2024/src/day1.rs
+++ 2024/src/day1.rs
@@ -1,5 +1,4 @@
 use crate::Conf;
-use core::{convert::Infallible, str::FromStr};
 use str_block::str_block;
 
 pub const INPUT: Conf<u32> = Conf::new(include_str!("day1.txt"), 2196996, 23655822);
@@ -16,16 +15,6 @@ pub const EX: Conf<u32> = Conf::new(
     11,
     31,
 );
-
-struct Part1 {
-    left: Vec<u32>,
-    right: Vec<u32>,
-}
-
-struct Part2 {
-    left: Vec<u32>,
-    right: Vec<u32>,
-}
 
 fn parse_line(s: &str) -> (u32, u32) {
     let mut l = 0;
@@ -47,51 +36,29 @@ fn parse_line(s: &str) -> (u32, u32) {
     (l, r)
 }
 
-impl FromStr for Part1 {
-    type Err = Infallible;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut left = Vec::with_capacity(1000);
-        let mut right = Vec::with_capacity(1000);
-        for line in s.lines() {
-            let (l, r) = parse_line(line);
-            left.push(l);
-            right.push(r);
-        }
-        Ok(Self { left, right })
-    }
-}
-
-impl FromStr for Part2 {
-    type Err = Infallible;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut left = Vec::with_capacity(1000);
-        let mut right = vec![0; 100_000];
-        for line in s.lines() {
-            let (l, r) = parse_line(line);
-            left.push(l);
-            right[r as usize] += 1;
-        }
-        Ok(Self { left, right })
-    }
-}
-
 pub fn part1(input: &str) -> u32 {
-    let mut input: Part1 = input.parse().unwrap();
-    input.left.sort_unstable();
-    input.right.sort_unstable();
-    input
-        .left
-        .into_iter()
-        .zip(input.right)
+    let mut left = Vec::with_capacity(1000);
+    let mut right = Vec::with_capacity(1000);
+    for line in input.lines() {
+        let (l, r) = parse_line(line);
+        left.push(l);
+        right.push(r);
+    }
+    left.sort_unstable();
+    right.sort_unstable();
+    left.into_iter()
+        .zip(right)
         .map(|(l, r)| l.abs_diff(r))
         .sum()
 }
 
 pub fn part2(input: &str) -> u32 {
-    let input: Part2 = input.parse().unwrap();
-    input
-        .left
-        .into_iter()
-        .map(|i| i * input.right[i as usize])
-        .sum()
+    let mut left = Vec::with_capacity(1000);
+    let mut right = vec![0; 100_000];
+    for line in input.lines() {
+        let (l, r) = parse_line(line);
+        left.push(l);
+        right[r as usize] += 1;
+    }
+    left.into_iter().map(|i| i * right[i as usize]).sum()
 }
