08a31f7274c6931ac334a0a22316da2bafeb7893
Maia <66437537+maia-s@users.noreply.github.com>
2024-12-03 16:01:30 +0100

    day 3 part 2

--- 2024/src/day3.rs
+++ 2024/src/day3.rs
@@ -1,16 +1,23 @@
-use core::str;
-
 use crate::Conf;
+use core::{hint::unreachable_unchecked, iter};
 use str_block::str_block;
 
-pub const INPUT: Conf<u32> = Conf::new(include_str!("day3.txt"), 181345830, 0);
+pub const INPUT: Conf<u32> = Conf::new(include_str!("day3.txt"), 181345830, 98729041);
 
 pub const EX: Conf<u32> = Conf::new(
     str_block! {"
         xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
     "},
     161,
-    0,
+    161,
+);
+
+pub const EX2: Conf<u32> = Conf::new(
+    str_block! {"
+        xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+    "},
+    161,
+    48,
 );
 
 fn mul(bytes: &[u8]) -> Option<u32> {
@@ -34,6 +41,22 @@ fn mul(bytes: &[u8]) -> Option<u32> {
     Some(l * r)
 }
 
+fn matches<'a>(bytes: &'a [u8], alts: &'a [&[u8]]) -> impl Iterator<Item = (usize, usize)> + 'a {
+    let mut i = 0;
+    iter::from_fn(move || {
+        #[allow(clippy::mut_range_bound)] // false positive
+        for j in i..bytes.len() {
+            for (alti, alt) in alts.iter().enumerate() {
+                if bytes[j..].starts_with(alt) {
+                    i = j + 1;
+                    return Some((alti, j));
+                }
+            }
+        }
+        None
+    })
+}
+
 pub fn part1(input: &str) -> u32 {
     input
         .lines()
@@ -51,5 +74,33 @@ pub fn part1(input: &str) -> u32 {
 }
 
 pub fn part2(input: &str) -> u32 {
-    0
+    let mut enabled = true;
+    input
+        .lines()
+        .map(|line| {
+            matches(line.as_bytes(), &[b"mul(", b"do()", b"don't()"])
+                .filter_map(|(alti, i)| match alti {
+                    0 => {
+                        if enabled {
+                            let i = i + 4;
+                            line[i..]
+                                .find(')')
+                                .and_then(|j| mul(&line.as_bytes()[i..i + j]))
+                        } else {
+                            None
+                        }
+                    }
+                    1 => {
+                        enabled = true;
+                        None
+                    }
+                    2 => {
+                        enabled = false;
+                        None
+                    }
+                    _ => unsafe { unreachable_unchecked() },
+                })
+                .sum::<u32>()
+        })
+        .sum()
 }
