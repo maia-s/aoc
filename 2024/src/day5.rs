f0dab2416cdc877ab1f643ac2e846fef9daf5a20
Maia <66437537+maia-s@users.noreply.github.com>
2024-12-06 00:15:36 +0100

    trying simd for the first time :o

--- 2024/src/day5.rs
+++ 2024/src/day5.rs
@@ -1,4 +1,8 @@
 use crate::{Conf, Input};
+use core::simd::{
+    cmp::{SimdOrd, SimdPartialOrd},
+    mask8x64, u8x64,
+};
 use str_block::str_block;
 
 pub const INPUT: Conf = Conf::new(
@@ -105,46 +109,45 @@ pub fn part1(input: &str) -> u32 {
 pub fn part2(input: &str) -> u32 {
     let (sec1, sec2) = input.split_once("\n\n").unwrap();
     let (sec1, sec2) = (sec1.as_bytes(), sec2.as_bytes().trim_ascii_end());
-    let mut map = [[0; 25]; 100];
+    let mut map = [[mask8x64::splat(false); 2]; 100];
     for line in sec1.split(|&b| b == b'\n') {
         let (a, b) = parse_a(line);
         let m = &mut map[b as usize];
-        m[0] += 1;
-        m[m[0] as usize] = a;
+        m[(a >= 64) as usize].set(a as usize & 63, true);
     }
     let mut sum = 0;
+    let vone = u8x64::splat(1);
     for mut line in sec2.split(|&b| b == b'\n') {
-        let mut order = [24; 100];
+        let mut order = [u8x64::splat(24); 2];
         let mut history = [0; 24];
         let mut reordered = false;
         let num = parse_b0(&mut line);
         history[0] = num;
         let m = map[num as usize];
-        for &di in &m[1..m[0] as usize + 1] {
-            order[di as usize] = 0;
-        }
+        order[0] = m[0].select(u8x64::default(), order[0]);
+        order[1] = m[1].select(u8x64::default(), order[1]);
         let mut i = 0;
         while let Some(num) = parse_b(&mut line) {
             i += 1;
-            let before_i = order[num as usize] as usize;
+            let before_i = order[(num >= 64) as usize][num as usize & 63] as usize;
             let ii = if before_i < 24 {
                 reordered = true;
                 let before_i_to = before_i + 1;
                 let hp = history.as_mut_ptr();
                 unsafe { hp.add(before_i).copy_to(hp.add(before_i_to), i - before_i) };
                 history[before_i] = num;
-                for i in order.iter_mut() {
-                    *i += (*i >= before_i as u8) as u8;
-                }
+                let vcmp = u8x64::splat(before_i as u8);
+                order[0] = order[0].simd_ge(vcmp).select(order[0] + vone, order[0]);
+                order[1] = order[1].simd_ge(vcmp).select(order[1] + vone, order[1]);
                 before_i as u8
             } else {
                 history[i] = num;
                 i as u8
             };
             let m = map[num as usize];
-            for &di in &m[1..m[0] as usize + 1] {
-                order[di as usize] = order[di as usize].min(ii);
-            }
+            let vii = u8x64::splat(ii);
+            order[0] = m[0].select(vii, order[0]).simd_min(order[0]);
+            order[1] = m[1].select(vii, order[1]).simd_min(order[1]);
         }
         if reordered {
             sum += history[i / 2] as u32;
