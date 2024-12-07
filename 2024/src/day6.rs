45567bb779ed50c7025d258d3ab1376b1e629ea2
Maia <66437537+maia-s@users.noreply.github.com>
2024-12-07 13:28:50 +0100

    remove part 2 wrapper type

--- 2024/src/day6.rs
+++ 2024/src/day6.rs
@@ -28,12 +28,8 @@ pub const EX: Conf = Conf::new(
     6,
 );
 
-#[repr(transparent)]
-#[derive(Clone, Copy, PartialEq, Eq)]
-struct B8(u8);
-
-struct Map<T> {
-    map: Vec<T>,
+struct Map<const PART: u8> {
+    map: Vec<u8>,
     width: u32,
     gx: i32,
     gy: i32,
@@ -41,7 +37,7 @@ struct Map<T> {
     result: u32,
 }
 
-impl<T> Map<T> {
+impl<const PART: u8> Map<PART> {
     #[inline(always)]
     fn height(&self) -> u32 {
         self.map.len() as u32 / self.width
@@ -53,18 +49,18 @@ impl<T> Map<T> {
     }
 
     #[inline(always)]
-    fn get(&self, (x, y): (i32, i32)) -> Option<&T> {
+    fn get(&self, (x, y): (i32, i32)) -> Option<&u8> {
         ((x as u32) < self.width && (y as u32) < self.height())
             .then(|| unsafe { self.get_unchecked((x, y)) })
     }
 
     #[inline(always)]
-    unsafe fn get_unchecked(&self, c: (i32, i32)) -> &T {
+    unsafe fn get_unchecked(&self, c: (i32, i32)) -> &u8 {
         unsafe { self.map.get_unchecked(self.coords_to_index_unchecked(c)) }
     }
 
     #[inline(always)]
-    unsafe fn get_unchecked_mut(&mut self, c: (i32, i32)) -> &mut T {
+    unsafe fn get_unchecked_mut(&mut self, c: (i32, i32)) -> &mut u8 {
         unsafe {
             let i = self.coords_to_index_unchecked(c);
             self.map.get_unchecked_mut(i)
@@ -72,7 +68,7 @@ impl<T> Map<T> {
     }
 }
 
-impl Map<u8> {
+impl Map<1> {
     fn new(input: &str) -> Self {
         let mut lines = input.as_bytes().trim_ascii_end().split(|&b| b == b'\n');
         let line = lines.next().unwrap();
@@ -124,27 +120,25 @@ impl Map<u8> {
     }
 }
 
-impl Map<B8> {
+impl Map<2> {
     fn new(input: &str) -> Self {
         let mut lines = input.as_bytes().trim_ascii_end().split(|&b| b == b'\n');
         let line = lines.next().unwrap();
         let width = line.len() as _;
         let mut map = Vec::with_capacity((width * width) as usize);
-        map.extend(line.iter().map(|&b| B8((b == b'#') as u8 * u8::MAX)));
+        map.extend(line.iter().map(|&b| (b == b'#') as u8 * u8::MAX));
         let mut gx = 0;
         let mut gy = 1;
         let mut got_start = false;
         for line in lines.by_ref() {
-            map.extend(line.iter().enumerate().map(|(x, &b)| {
-                B8(match b {
-                    b'#' => u8::MAX,
-                    b'^' => {
-                        got_start = true;
-                        gx = x as i32;
-                        0
-                    }
-                    _ => 0,
-                })
+            map.extend(line.iter().enumerate().map(|(x, &b)| match b {
+                b'#' => u8::MAX,
+                b'^' => {
+                    got_start = true;
+                    gx = x as i32;
+                    0
+                }
+                _ => 0,
             }));
             if got_start {
                 break;
@@ -152,7 +146,7 @@ impl Map<B8> {
             gy += 1;
         }
         for line in lines {
-            map.extend(line.iter().map(|&b| B8((b == b'#') as u8 * u8::MAX)));
+            map.extend(line.iter().map(|&b| (b == b'#') as u8 * u8::MAX));
         }
         Self {
             map,
@@ -164,7 +158,7 @@ impl Map<B8> {
         }
     }
 
-    fn loops(&self, m: &mut [B8]) -> bool {
+    fn loops(&self, m: &mut [u8]) -> bool {
         m.copy_from_slice(&self.map);
         let height = self.height();
         let mut dir = self.dir;
@@ -172,15 +166,15 @@ impl Map<B8> {
         let (mut gx, mut gy) = (self.gx, self.gy);
         while (gx as u32) < self.width && (gy as u32) < height {
             let gtile = unsafe { m.get_unchecked_mut(self.coords_to_index_unchecked((gx, gy))) };
-            if (gtile.0 as i8) < 0 {
+            if (*gtile as i8) < 0 {
                 gx -= dx;
                 gy -= dy;
                 dir = dir.rotate_cw();
                 (dx, dy) = dir.delta();
-            } else if (gtile.0 & dir.bit()) != 0 {
+            } else if (*gtile & dir.bit()) != 0 {
                 return true;
             } else {
-                gtile.0 |= dir.bit();
+                *gtile |= dir.bit();
                 gx += dx;
                 gy += dy;
             }
@@ -189,8 +183,8 @@ impl Map<B8> {
     }
 }
 
-impl<T> Index<(i32, i32)> for Map<T> {
-    type Output = T;
+impl<const PART: u8> Index<(i32, i32)> for Map<PART> {
+    type Output = u8;
 
     #[inline(always)]
     fn index(&self, (x, y): (i32, i32)) -> &Self::Output {
@@ -198,7 +192,7 @@ impl<T> Index<(i32, i32)> for Map<T> {
     }
 }
 
-impl<T> IndexMut<(i32, i32)> for Map<T> {
+impl<const PART: u8> IndexMut<(i32, i32)> for Map<PART> {
     #[inline(always)]
     fn index_mut(&mut self, (x, y): (i32, i32)) -> &mut Self::Output {
         let i = self.coords_to_index_unchecked((x, y));
@@ -206,8 +200,8 @@ impl<T> IndexMut<(i32, i32)> for Map<T> {
     }
 }
 
-impl<T> Index<(u32, u32)> for Map<T> {
-    type Output = T;
+impl<const PART: u8> Index<(u32, u32)> for Map<PART> {
+    type Output = u8;
 
     #[inline(always)]
     fn index(&self, (x, y): (u32, u32)) -> &Self::Output {
@@ -215,7 +209,7 @@ impl<T> Index<(u32, u32)> for Map<T> {
     }
 }
 
-impl<T> IndexMut<(u32, u32)> for Map<T> {
+impl<const PART: u8> IndexMut<(u32, u32)> for Map<PART> {
     #[inline(always)]
     fn index_mut(&mut self, (x, y): (u32, u32)) -> &mut Self::Output {
         let i = self.coords_to_index_unchecked((x as i32, y as i32));
@@ -251,35 +245,33 @@ impl Dir {
 }
 
 pub fn part1(input: &str) -> u32 {
-    let mut map = Map::<u8>::new(input);
+    let mut map = Map::<1>::new(input);
     while map.step() {}
     map.result
 }
 
 pub fn part2(input: &str) -> u32 {
-    let mut map = Map::<B8>::new(input);
-    let mut scratch = vec![B8(0); map.map.len()];
+    let mut map = Map::<2>::new(input);
+    let mut scratch = vec![0; map.map.len()];
     let mut candidates = 0;
 
     let height = map.height();
     let (mut dx, mut dy) = map.dir.delta();
     loop {
-        if (unsafe { map.get_unchecked_mut((map.gx, map.gy)) }.0 as i8) < 0 {
+        if (*unsafe { map.get_unchecked_mut((map.gx, map.gy)) } as i8) < 0 {
             map.gx -= dx;
             map.gy -= dy;
             map.dir = map.dir.rotate_cw();
             (dx, dy) = map.dir.delta();
         } else {
-            unsafe { map.get_unchecked_mut((map.gx, map.gy)) }.0 |= map.dir.bit();
+            *unsafe { map.get_unchecked_mut((map.gx, map.gy)) } |= map.dir.bit();
             map.gx += dx;
             map.gy += dy;
             if (map.gx as u32) < map.width && (map.gy as u32) < height {
-                let tile = mem::replace(
-                    unsafe { map.get_unchecked_mut((map.gx, map.gy)) },
-                    B8(u8::MAX),
-                );
-                candidates += (tile.0 == 0 && map.loops(&mut scratch)) as u32;
-                unsafe { map.get_unchecked_mut((map.gx, map.gy)) }.0 = tile.0;
+                let tile =
+                    mem::replace(unsafe { map.get_unchecked_mut((map.gx, map.gy)) }, u8::MAX);
+                candidates += (tile == 0 && map.loops(&mut scratch)) as u32;
+                *unsafe { map.get_unchecked_mut((map.gx, map.gy)) } = tile;
             } else {
                 break;
             }
