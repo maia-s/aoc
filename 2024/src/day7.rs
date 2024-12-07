ea5e229e61c274690cdb56ff1a7679dedcdade67
Maia <66437537+maia-s@users.noreply.github.com>
2024-12-07 09:17:08 +0100

    day 7 part 2

--- 2024/src/day7.rs
+++ 2024/src/day7.rs
@@ -1,10 +1,12 @@
+#![allow(clippy::enum_variant_names)]
+
 use crate::{Conf, Input};
 use str_block::str_block;
 
 pub const INPUT: Conf<u64> = Conf::new(
     Input::FileHash("30e1c6a2e8425ba49f7194d019dde4f9179e6994c68681ae0c48af82c26c29b4"),
     267566105056,
-    0,
+    116094961956019,
 );
 
 pub const EX: Conf<u64> = Conf::new(
@@ -20,13 +22,20 @@ pub const EX: Conf<u64> = Conf::new(
         292: 11 6 16 20
     "}),
     3749,
-    0,
+    11387,
 );
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Op {
     MulFrom(u64),
     AddFrom(u64),
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Op2 {
+    MulFrom(u64),
+    AddFrom(u64),
+    CatFrom(u64),
 }
 
 fn int(input: &mut &[u8]) -> Option<u64> {
@@ -54,6 +63,11 @@ fn parse_line(mut input: &[u8], out: &mut Vec<u32>) -> Option<u64> {
             out.push(int as u32);
         }
     })
+}
+
+#[inline(always)]
+fn cat(l: u64, r: u64) -> u64 {
+    (l * 10_u64.pow(r.ilog10() + 1)) + r
 }
 
 pub fn part1(input: &str) -> u64 {
@@ -101,5 +115,72 @@ pub fn part1(input: &str) -> u64 {
 }
 
 pub fn part2(input: &str) -> u64 {
-    0
+    let mut nums = Vec::with_capacity(12);
+    let mut sums = Vec::with_capacity(12);
+    let mut total = 0;
+    'lines: for line in input.as_bytes().trim_ascii_end().split(|&b| b == b'\n') {
+        if let Some(target) = parse_line(line, &mut nums) {
+            let mut sum = nums[0] as u64;
+            sums.clear();
+            'sums: while sums.len() + 1 < nums.len() {
+                let last = sums.len() + 2 == nums.len();
+                let arg = nums[sums.len() + 1] as u64;
+                let s = sum * arg;
+                if if last { s == target } else { s <= target } {
+                    sums.push(Op2::MulFrom(sum));
+                    sum = s;
+                } else {
+                    let s = sum + arg;
+                    if if last { s == target } else { s <= target } {
+                        sums.push(Op2::AddFrom(sum));
+                        sum = s;
+                    } else {
+                        let s = cat(sum, arg);
+                        if if last { s == target } else { s <= target } {
+                            sums.push(Op2::CatFrom(sum));
+                            sum = s;
+                        } else {
+                            while let Some(op) = sums.pop() {
+                                match op {
+                                    Op2::MulFrom(psum) => {
+                                        sum = psum;
+                                        let arg = nums[sums.len() + 1] as u64;
+                                        let s = sum + arg;
+                                        if s <= target {
+                                            sums.push(Op2::AddFrom(sum));
+                                            sum = s;
+                                        } else {
+                                            let s = cat(sum, arg);
+                                            if s <= target {
+                                                sums.push(Op2::CatFrom(sum));
+                                                sum = s;
+                                            } else {
+                                                continue;
+                                            }
+                                        }
+                                        continue 'sums;
+                                    }
+                                    Op2::AddFrom(psum) => {
+                                        sum = psum;
+                                        let s = cat(sum, nums[sums.len() + 1] as u64);
+                                        if s <= target {
+                                            sums.push(Op2::CatFrom(sum));
+                                            sum = s;
+                                        } else {
+                                            continue;
+                                        }
+                                        continue 'sums;
+                                    }
+                                    _ => (),
+                                }
+                            }
+                            continue 'lines;
+                        }
+                    }
+                }
+            }
+            total += target;
+        }
+    }
+    total
 }
