5b32eba1fe2ee084db78c06a931d57e4253dd53a
Maia <66437537+maia-s@users.noreply.github.com>
2024-12-08 07:05:46 +0100

    day 8 part 2

--- 2024/src/day8.rs
+++ 2024/src/day8.rs
@@ -5,7 +5,7 @@ use str_block::str_block;
 pub const INPUT: Conf = Conf::new(
     Input::FileHash("8b278ec0816291e953d8e2e60113c4f1c1b6d76634ce8a64d9eda1f7702cfd9d"),
     311,
-    0,
+    1115,
 );
 
 pub const EX: Conf = Conf::new(
@@ -24,7 +24,7 @@ pub const EX: Conf = Conf::new(
         ............
     "}),
     14,
-    0,
+    34,
 );
 
 pub fn part1(input: &str) -> u32 {
@@ -67,5 +67,42 @@ pub fn part1(input: &str) -> u32 {
 }
 
 pub fn part2(input: &str) -> u32 {
-    0
+    let mut map: [_; 0x50] = array::from_fn(|_| Vec::new());
+    let mut anti = [false; 0x10000];
+    let mut count = 0;
+    let mut width = 0;
+    let mut height = 0;
+    for line in input.as_bytes().trim_ascii_end().split(|&b| b == b'\n') {
+        width = 0;
+        for b in line.iter().copied() {
+            if b >= b'0' {
+                map[(b - b'0') as usize].push((width as i8, height as i8));
+            }
+            width += 1;
+        }
+        height += 1;
+    }
+    for locs in map {
+        for (i, (ax, ay)) in locs.iter().enumerate() {
+            for (bx, by) in locs[i + 1..].iter() {
+                let dx = ax - bx;
+                let dy = ay - by;
+                let mut hx = ax + dx;
+                let mut hy = ay + dy;
+                while (hx as u8) < width && (hy as u8) < height {
+                    hx += dx;
+                    hy += dy;
+                }
+                hx -= dx;
+                hy -= dy;
+                while (hx as u8) < width && (hy as u8) < height {
+                    let i = (hx as u8 as usize) << 8 | hy as u8 as usize;
+                    count += !mem::replace(&mut anti[i], true) as u32;
+                    hx -= dx;
+                    hy -= dy;
+                }
+            }
+        }
+    }
+    count
 }
